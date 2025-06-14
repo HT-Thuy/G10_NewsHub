520efa53d4faa486ad38f2b300c025177fa0a3a5
HT-Thuy <112024654+HT-Thuy@users.noreply.github.com>
2025-06-15 01:50:29 +0700

    day code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,20 +58,15 @@ dependencies {
     // ViewModel and LiveData for MVVM
     implementation("androidx.lifecycle:lifecycle-viewmodel:2.7.0")
     implementation("androidx.lifecycle:lifecycle-livedata:2.7.0")
-
-// Retrofit for networking
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
-// OkHttp (thường đi kèm Retrofit, nhưng thêm để tường minh)
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
-
-// Glide for loading images
     implementation("com.github.bumptech.glide:glide:4.16.0")
-
-// Material Design Components
     implementation("com.google.android.material:material:1.12.0")
-
-// RecyclerView
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+    // Room for offline
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    // Sử dụng annotationProcessor thay vì kapt cho dự án Java
+    annotationProcessor("androidx.room:room-compiler:$room_version")
 }
