ed758dee8aec79f6972104ac2f24a99446e6893e
HT-Thuy <112024654+HT-Thuy@users.noreply.github.com>
2025-06-15 02:38:21 +0700

    day code

---
+++ app/src/main/java/com/example/g10_newshub/utils/SessionManager.java
@@ -0,0 +1,41 @@
+package com.example.g10_newshub.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SessionManager {
+    private static final String PREF_NAME = "NewsHubSession";
+    private static final String KEY_IS_LOGGED_IN = "isLoggedIn";
+    private static final String KEY_USER_ID = "userId";
+    private static final String KEY_USERNAME = "username";
+
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private Context _context;
+
+    public SessionManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        editor = pref.edit();
+    }
+
+    public void createLoginSession(int userId, String username) {
+        editor.putBoolean(KEY_IS_LOGGED_IN, true);
+        editor.putInt(KEY_USER_ID, userId);
+        editor.putString(KEY_USERNAME, username);
+        editor.commit();
+    }
+
+    public boolean isLoggedIn() {
+        return pref.getBoolean(KEY_IS_LOGGED_IN, false);
+    }
+
+    public void logoutUser() {
+        editor.clear();
+        editor.commit();
+    }
+
+    public int getUserId() {
+        return pref.getInt(KEY_USER_ID, -1);
+    }
+}
